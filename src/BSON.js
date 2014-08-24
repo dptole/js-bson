9e4ad24c4536a780f5bbfd22869ca8051bafe6c1
dptole <danillo.paiva.toledo@gmail.com>
2014-08-24 18:05:24 -0300

    Encode/decode.

--- src/BSON.js
+++ src/BSON.js
@@ -170,7 +170,173 @@
     }
 
     return sign * mantissa * Math.pow(2, exponent - 52);
-  }
+  };
+
+  /* ********************************************************************** */
+
+  function encode( doc ) {
+    return encode.core( doc, Object.getOwnPropertyNames( doc ), '', 0 );
+  };
+  encode.core = function( document, properties, bson, i ) {
+    for(; i < properties.length; i++)
+      bson += ENCODE_FUNCTIONS[ typeof( document[ properties[ i ] ] ) ](
+        properties[ i ],
+        document[ properties[ i ] ]
+      );
+
+    return readAsInt32LE( bson.length + 5 ) + bson + '\x00';
+  };
+
+  /* ********************************************************************** */
+
+  function decode( buffer, is_array ) {
+    var offset = 0
+      , key = null
+      , type = null
+      , decoded = is_array ? [] : {}
+      , length = buffer.readInt32LE( offset )
+    ;
+
+    if( length !== buffer.length )
+      throw new Error(
+        'BSON header error, got '
+        + buffer.length + ' but expected ' + length + '.'
+      );
+    offset += 4;
+
+    while( offset < length ) {
+      type = buffer.pick( offset++ );
+
+      key = is_array
+        ? is_array++ - 1
+        : String.fromCharCode.apply(
+            String,
+            buffer.sliceWhile(
+              function( octect ) { return octect !== 0; }, offset
+            ).toArray()
+          )
+      ;
+      offset += key.toString().length + 1;
+      switch( type ) {
+        case 0: break;
+
+        case TYPES.JS_CODE_W_S.D:
+          var data_type_length = buffer.readInt32LE( offset )
+            
+            , code_length_offset = offset + 4
+            , code_length = buffer.readInt32LE( code_length_offset )
+            
+            , code_offset = code_length_offset + 4
+            , code = String.fromCharCode.apply(
+                String,
+                buffer.toArray( code_offset, code_length - 1 )
+              )
+            
+            , variables_length_offset = code_offset + code.length + 1
+            , variables_length = buffer.readInt32LE( variables_length_offset )
+            , variables_encoded = String.fromCharCode.apply(
+                String,
+                buffer.toArray( variables_length_offset, variables_length )
+              )
+            , variables = BSON.decode( variables_encoded )
+          ;
+
+          decoded[ key ] = createFunction( code, variables );
+          offset += data_type_length;
+        break;
+
+        case TYPES.JS_CODE.D:
+          var code_length = buffer.readInt32LE( offset )
+            , code = String.fromCharCode.apply(
+                String,
+                buffer.toArray( offset + 4, code_length - 1 )
+              )
+          ;
+          decoded[ key ] = Function( code );
+          offset += code_length + 4;
+        break;
+
+        case TYPES.BINARY.D:
+        case TYPES.STRING.D:
+          var start = offset + 4 + ( TYPES.BINARY.D === type )
+            , end =
+                buffer.readInt32LE( offset )
+                - (TYPES.STRING.D === type)
+          ;
+
+          decoded[ key ] = String.fromCharCode.apply(
+            String, buffer.toArray( start, end )
+          );
+
+          if( TYPES.BINARY.D === type )
+            decoded[ key ] = Binary(
+              decoded[ key ],
+              buffer.pick( offset + 4 )
+            );
+
+          offset += decoded[ key ].length + 4;
+        break;
+
+        case TYPES.DOCUMENT.D:
+        case TYPES.ARRAY.D:
+          var _offset = buffer.readInt32LE( offset );
+          decoded[ key ] = decode(
+            buffer.slice( offset, _offset ), TYPES.ARRAY.D === type
+          );
+          offset += _offset - 1;
+        break;
+
+        case TYPES.BOOLEAN.D:
+          decoded[ key ] = !!buffer.pick( offset );
+        break;
+
+        case TYPES.UNDEFINED.D:
+        case TYPES.NULL.D:
+          decoded[ key ] = TYPES.NULL.D === type ? null : undefined;
+          offset--;
+        break;
+
+        case TYPES.INT32.D:
+          decoded[ key ] = buffer.readInt32LE( offset, 4 ); offset += 3;
+        break;
+
+        case TYPES.UTC_DATETIME.D:
+        case TYPES.INT64.D:
+          decoded[ key ] = buffer.readInt64LE( offset );
+          if( TYPES.UTC_DATETIME.D === type )
+            decoded[ key ] = new Date( decoded[ key ] );
+          offset += 7;
+        break;
+
+        case TYPES.REGEXP.D:
+          var source = buffer.sliceWhile(
+                function( octect ) { return octect !== 0; }, offset
+              ).toArray()
+            , modifiers = buffer.sliceWhile(
+                function( octect ) { return octect !== 0; }
+                , offset + source.length + 1
+              ).toArray()
+          ;
+          decoded[ key ] = new RegExp(
+            String.fromCharCode.apply( String, source ),
+            String.fromCharCode.apply( String, modifiers )
+          );
+          offset += source.length + modifiers.length + 1;
+        break;
+
+        case TYPES.DOUBLE.D:
+          decoded[ key ] = buffer.readFloat64LE( offset );
+          offset += 7;
+        break;
+
+        default: throw new Error('Unknown type ' + type );
+      }
+
+      offset++;
+    }
+
+    return decoded;
+  };
 
   /* ********************************************************************** */
 
