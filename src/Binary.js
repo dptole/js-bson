8ad8270d393611e76b91437f792b5b01389f1897
dptole <danillo.paiva.toledo@gmail.com>
2014-10-09 03:35:53 -0300

    Add comments.

--- src/Binary.js
+++ src/Binary.js
@@ -1,4 +1,19 @@
 -function( global ) {
+  /* 
+  Binary( data, subtype ) -> instanceof Binary
+
+  A simple way to represent the BSON binary data in JavaScript.
+
+  data -> String
+  subtype -> Number
+
+  Ex.:
+    var bin = Binary('binary-data', 4);
+    bin.toString().split('').map(function(c){return c.charCodeAt(0)})
+    // [98, 105, 110, 97, 114, 121, 45, 100, 97, 116, 97]
+    console.log(bin.subtype); // 4
+
+  */
   function Binary( data, subtype ) {
     if( ! ( this instanceof Binary ) )
       return new Binary( data, subtype );
--- standalone/BSON.js
+++ standalone/BSON.js
@@ -127,6 +127,21 @@
 
   /* ********************************************************************** */
 
+  /* 
+  Binary( data, subtype ) -> instanceof Binary
+
+  A simple way to represent the BSON binary data in JavaScript.
+
+  data -> String
+  subtype -> Number
+
+  Ex.:
+    var bin = Binary('binary-data', 4);
+    bin.toString().split('').map(function(c){return c.charCodeAt(0)})
+    // [98, 105, 110, 97, 114, 121, 45, 100, 97, 116, 97]
+    console.log(bin.subtype); // 4
+
+  */
   function Binary( data, subtype ) {
     if( ! ( this instanceof Binary ) )
       return new Binary( data, subtype );
